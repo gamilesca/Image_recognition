e4870646537e23f72eb70d9b5305f23ab3b6824f
Gabriela Catalán Medina <66983825+gamilesca@users.noreply.github.com>
2021-03-23 12:07:32 +0100

    Create HPC_classifier_3.py

---
+++ HPC_classifier_3.py
@@ -0,0 +1,117 @@
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import tensorflow as tf
+import numpy as np
+import os
+
+from itertools import cycle
+
+
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import label_binarize
+from sklearn.multiclass import OneVsRestClassifier
+from scipy import interp
+from sklearn.metrics import roc_auc_score
+
+from keras.preprocessing import image
+
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+# All images will be rescaled by 1./255
+train_datagen = ImageDataGenerator(rescale=1/255)
+validation_datagen = ImageDataGenerator(rescale=1/255)
+
+# Flow training images in batches of 120 using train_datagen generator
+train_generator = train_datagen.flow_from_directory(
+        '/scratch/dcas/n.drougard/img_recog/train/',  # This is the source directory for training images
+        classes = ['training_dataset_y+', 'training_dataset_y-'],
+        target_size=(200, 200),  # All images will be resized to 200x200
+        batch_size=64,
+        # Use binary labels
+        class_mode='binary')
+
+print(train_generator)
+
+# Flow validation images in batches of 19 using valid_datagen generator
+validation_generator = validation_datagen.flow_from_directory(
+        '/scratch/dcas/n.drougard/img_recog/valid/',  # This is the source directory for training images
+        classes = ['validation_dataset_y+', 'validation_dataset_y-'],
+        target_size=(200, 200),  # All images will be resized to 200x200
+        batch_size=32,
+        # Use binary labels
+        class_mode='binary',
+        shuffle=False)
+
+print(validation_generator)
+
+print('-------------------------------')
+#Model
+
+def create_model():
+     model = tf.keras.models.Sequential([
+                                    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(200, 200, 3)),
+                                    tf.keras.layers.MaxPooling2D(2, 2),
+                                    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(200, 200, 3)),
+                                    tf.keras.layers.MaxPooling2D(2, 2),
+                                    #tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+                                    #tf.keras.layers.MaxPooling2D(2,2),
+                                    tf.keras.layers.Flatten(input_shape = (200,200,3)), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(1, activation=tf.nn.sigmoid)])
+
+     model.compile(optimizer = tf.optimizers.Adam(),
+              loss = 'binary_crossentropy',
+              metrics=['accuracy'])
+
+     return model
+
+# Create a basic model instance
+model = create_model()
+
+# Display the model's architecture
+model.summary()
+
+checkpoint_path = "/scratch/dcas/n.drougard/img_recog/ckpt/"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create a callback that saves the model's weights
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+
+history = model.fit(train_generator,
+      #steps_per_epoch=8,  
+      epochs=200,
+      verbose=1,
+      validation_data = validation_generator,
+      #validation_steps=8, 
+      callbacks=[cp_callback])
+
+STEP_SIZE_TEST=validation_generator.n//validation_generator.batch_size
+validation_generator.reset()
+preds = model.predict(validation_generator,
+                      verbose=1)
+
+
+model.evaluate(validation_generator)
+
+fpr, tpr, _ = roc_curve(validation_generator.classes, preds)
+roc_auc = auc(fpr, tpr)
+
+plt.figure()
+lw = 2
+plt.plot(fpr, tpr, color='darkorange',
+         lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver Operating Characteristic')
+plt.legend(loc="lower right")
+plt.savefig('/scratch/dcas/n.drougard/img_recog/fig.png')
