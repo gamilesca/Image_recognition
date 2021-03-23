195206d68df76aea2607f9e7cd0cf2a8a71dfbb3
Gabriela Catalán Medina <66983825+gamilesca@users.noreply.github.com>
2021-03-23 20:43:18 +0100

    Update HPC_classifier_4.py

--- HPC_classifier_4.py
+++ HPC_classifier_4.py
@@ -55,8 +55,6 @@ def create_model():
      model = tf.keras.models.Sequential([
                                     tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(200, 200, 3)),
                                     tf.keras.layers.MaxPooling2D(2, 2),
-                                    tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(200, 200, 3)),
-                                    tf.keras.layers.MaxPooling2D(2, 2),
                                     #tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
                                     #tf.keras.layers.MaxPooling2D(2,2),
                                     tf.keras.layers.Flatten(input_shape = (200,200,3)), 
